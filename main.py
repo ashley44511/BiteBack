c59f8f8aaf9b74a02592255a3f79e252412066c8
Tyler <tyler.audino@ufl.edu>
2024-04-16 17:33:23 -0400

    loading pickle files works in main

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # run when user submits their five food items
 import pickle
-import graphPickle 
+from graphPickle import Graph
+from hashPickle import Hash
 # script with algorithms
+graph = Graph()
+hash = Hash()
 
+# get data into classes from pickle file
 with open("./data/data_graph.pickle", "rb") as graphFile:
     graph = pickle.load(graphFile)
-#graph = pickle.load(open("data/data_graph.pickle", "rb"))
-#hash = pickle.load(open("data/data_hash.pickle", "rb"))
+with open("./data/data_hash.pickle", "rb") as hashFile:
+    hash = pickle.load(hashFile)
+
+graphFile.close()
+hashFile.close()
 
 if __name__ == "__main__":
     graph.print_graph()
