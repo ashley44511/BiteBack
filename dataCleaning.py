a8b8e7c569a5c44aed18b36ee046b349c9b4a5e1
Tikiri <ekanayake.tikiri@ufl.edu>
2024-04-18 18:55:15 -0400

    made and loaded data into hash table

--- dataCleaning.py
+++ dataCleaning.py
@@ -11,5 +11,5 @@ remove = 'Category'
 remove_again = 'Nutrient Data Bank Number'
 df.drop(remove, axis=1, inplace=True)
 df.drop(remove_again, axis=1, inplace=True)
-df = df.head()
-df.head() 
+
+# we ready to game
--- hashPickle.py
+++ hashPickle.py
@@ -1,15 +1,61 @@
 import pickle
 
-# hash class here
+# Referenced Geeks for Geeks for Hash Table implementation
+# https://www.geeksforgeeks.org/implementation-of-hash-table-in-python-using-separate-chaining/
+# changed it up though since we don't need all of their functions and used open addressing instead
+
+# how each food is stored in the HT
+class Entry:
+    def __init__(self, food, nutrients):
+        self.food = food
+        self.nutrients = nutrients  
+    # can maybe make a compare function here that can calculate the nutritional differences
+
 class Hash:
+    # constructor for HT
     def __init__(self):
-        pass
+        self.size = 0
+        self.table = [None] * 7083
+        # 7083 rows in the table
+        
+    # inserting into HT
+    def insert(self, food, nutrition):
+        index = hash(food) % 7082
+        
+        if self.table[index] is None:
+            self.table[index] = Entry(food, nutrition)
+            self.size += 1
+        else:
+            # linear probing is simpler to implement and more space efficient
+            while self.table[index] is not None:
+                index += 1
+                if index == 7083:
+                    index = 0
+            self.table[index] = Entry(food, nutrition)
+            self.size += 1
+        
+    # searching through HT to find food, given its name
+    def search(self, food):
+        index = hash(food) % 7082
+        while self.table[index].food != food:
+            index += 1
+            if index == 7083:
+                index = 0
+        
+        return self.table[index].nutrients
+        # returning the entry itself. can alter to return only the nutrients
 
 
 
 if __name__ == "__main__":
-
     hash = Hash()
+    # iterate through each row of dataframe and store food name + nutritional content
+    for index, row, in df.iterrows():
+        food = row.iloc[0]
+        nutrition = {}
+        for nutrient, amount in zip(df.columns[1:], row.iloc[1:]):
+            nutrition[nutrient] = amount
+        hash.insert(food, nutrition)
 
     # after hash data is loaded and saved
     with open("data/data_hash.pickle", "wb") as file:
