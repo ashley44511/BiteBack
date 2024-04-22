f602417f764359c960be534e4547a8b531c67d6e
Tyler <tyler.audino@ufl.edu>
2024-04-22 10:00:21 -0400

    finished graph/chart developemnt

--- bite-back-project/flask_react/backend/main.py
+++ bite-back-project/flask_react/backend/main.py
@@ -6,6 +6,7 @@ from hashPickle import HashTable, pickleHash
 from hashPickle import Entry
 import plotly.express as px
 import plotly.io as pio
+from plotly import graph_objs as go
 import time
 
 #from flask import Flask, request, jsonify
@@ -61,7 +62,7 @@ class Main:
         # return 5 suggested foods - END HASH TIME
         endTime = time.time()
         runTime = endTime - startTime #calculate runTime of hash 
-        return runTime, foodSuggestions
+        return runTime, foodSuggestions, goalNutrients
 
     def runGraph(self, meal):
         # GRAPH - start time here
@@ -94,13 +95,39 @@ class Main:
         labels = list(data.keys())
         values = list(data.values())
         fig = px.pie(values=values, names=labels, title='Input Meal Macronutrient Distribution', color_discrete_sequence=px.colors.diverging.Fall)
-        pio.write_image(fig, "../public/macro_pie_chart.jpg")
+        fig.update_layout(title_x=0.5)
+        fig.update_layout(width=800, height=600)
+        fig.update_layout(title_font=dict(size=24), legend_font=dict(size=16))
+        fig.write_image("../public/macro_pie_chart.png")
         #fig.write_image("bite-back-project/flask_react/public/macro_pie_chart.jpg", engine="plotly")
         return
     
-    def createHistogram(self,):
-        # this function will take in 
-        pass
+    def createBarChart(self, nutrients, goalNutrients):
+        # this function will take in the nutrients of the input meal and the goal nutrients (assuming 1/3 of DV should be met by meal) and will show gap of nutreients in terms of percent
+        categories = []
+        percentage = []
+        for nutrient in nutrients:
+            if goalNutrients[nutrient] == 0:
+                # skip nutrients with no DV
+               continue
+            else:
+                percentage.append((nutrients[nutrient]/goalNutrients[nutrient]) * 100)
+                categories.append(nutrient)
+
+        # Create overlapped bar chart
+        fig = px.bar(x=categories, y=percentage, barmode='overlay', labels={'x':'Nutrient', 'y':'Percent Met'},
+                    title='Percentage of 1/3 Daily Value Met from Input Meal - Excludes Nutrients with no DV', color_discrete_sequence=['green'])
+
+        goal_value = 100
+        fig.add_hline(y=goal_value, line_dash='dash', line_color='red', annotation_text=f'Goal: {goal_value}',
+                      annotation_position='bottom right')
+
+        fig.update_layout(showlegend=False)
+        fig.update_layout(title_x=0.5)
+        fig.update_layout(width=800, height=600)
+
+        # Save the chart as an image
+        fig.write_image('../public/overlapped_bar_chart.jpg')  # Change file format as needed (e.g., 'overlapped_bar_chart.jpg')
 
     def mainImportVersion(self):
         # get input from website as args
@@ -114,7 +141,7 @@ class Main:
         self.unpickleHash()
 
         # each functions compares the time it took and the foods suggested by each data structure
-        hashTime, hashSuggestions = self.runHash(input)
+        hashTime, hashSuggestions, goalNutrients = self.runHash(input)
         graphTime, graphSuggestions, mealNutrition = self.runGraph(input)
         print("Hash RunTime: " + str(hashTime))
         print("Hash Suggestions: ", end="")
@@ -125,14 +152,10 @@ class Main:
 
         #create visualizations - saved in /public to be displayed 
         self.createPieChart(mealNutrition)
+        self.createBarChart(mealNutrition, goalNutrients)
 
-        #return hash runtime, graph runtime, graph suggestions, hash suggestions
+        #return hash runtime, graph runtime, graph suggestions, hash suggestions - used by Front End
         return graphTime, graphSuggestions, hashTime, hashSuggestions
-
-        
-        
-
-        # send charts, time, and food suggestions to back end 
 
 '''
 @api.route('/profile/', methods=['POST'])
@@ -170,7 +193,6 @@ def my_profile():
     """
 
     return response_body
-
 '''
 
 if __name__ == "__main__":
